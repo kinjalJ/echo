2f37e1c9659655e751e73ad0958e1a4e0b88da3a
ConfidentGovernance <devcg4@confidentgovernance.com>
2014-09-12 07:11:41 +0530

    give date

--- echo.php
+++ echo.php
@@ -7,6 +7,7 @@ echo "<h2>PHP is fun!</h2>";
 echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This", " string", " was", " made", " with multiple parameters.";
+echo "<h2>PHP is fun!</h2>";
 ?> 
 
 </body>
